cd0afe12fdee5d91bcaa129d12b0898eca2a8ef5
dev_ <jhondavefuentes9@gmail.com>
2025-05-12 13:40:22 +0800

    Add files via upload

--- ProjectDev/main.java
+++ ProjectDev/main.java
@@ -1,15 +1,11 @@
 package ProjectDev;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class main {
-    private static List<Person> people = new ArrayList<>();
-    private static int nextId = 1;
     private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
+        
         boolean running = true;
         
         while (running) {
@@ -17,7 +13,7 @@ public class main {
             int choice = menu.getMenuChoice(scanner); 
             scanner.nextLine();
             menuHandler.handleMenuChoice(choice);
-        }
+         }
         scanner.close();
     }
 }
